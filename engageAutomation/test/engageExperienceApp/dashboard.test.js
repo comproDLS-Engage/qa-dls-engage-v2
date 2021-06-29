9f14e09a9df17e1aa75fb76c1b7ab3b72b79f552
meghagarg <meghagarg.ymca@gmail.com>
2021-06-29 15:14:21 +0530

    Book dropdown

--- engageAutomation/test/engageExperienceApp/dashboard.test.js
+++ engageAutomation/test/engageExperienceApp/dashboard.test.js
@@ -7,7 +7,7 @@ module.exports = {
 
     //Validate on clicking "Interactive Activities" resource in Explore menu, same tab is launched on Browse
     ENG_DASH_TC_1: function () {
-        sts = dashboardPage.clickResource("Interactive Activities");
+        sts = dashboardPage.clickExploreResource("Interactive Activities");
         assertion.assertEqual(sts, true, "Dashboard page status mismatch");
         sts = browsePage.isInitialized();
         assertion.assertEqual(sts.pageStatus, true, "Browse page status mismatch");
@@ -17,7 +17,7 @@ module.exports = {
 
     //Validate on clicking "Videos" resource in Explore menu, same tab is launched on Browse
     ENG_DASH_TC_2: function () {
-        sts = dashboardPage.clickResource("Videos");
+        sts = dashboardPage.clickExploreResource("Videos");
         assertion.assertEqual(sts, true, "Dashboard page status mismatch");
         sts = browsePage.isInitialized();
         assertion.assertEqual(sts.pageStatus, true, "Browse page status mismatch");
@@ -27,7 +27,7 @@ module.exports = {
 
     //Validate on clicking "Online Classes" resource in Explore menu, same tab is launched on Browse
     ENG_DASH_TC_3: function () {
-        sts = dashboardPage.clickResource("Online Classes");
+        sts = dashboardPage.clickExploreResource("Online Classes");
         assertion.assertEqual(sts, true, "Dashboard page status mismatch");
         sts = browsePage.isInitialized();
         assertion.assertEqual(sts.pageStatus, true, "Browse page status mismatch");
@@ -37,7 +37,7 @@ module.exports = {
 
     //Validate on clicking "Teacher Training" resource in Explore menu, same tab is launched on Browse
     ENG_DASH_TC_4: function () {
-        sts = dashboardPage.clickResource("Teacher Training");
+        sts = dashboardPage.clickExploreResource("Teacher Training");
         assertion.assertEqual(sts, true, "Dashboard page status mismatch");
         sts = browsePage.isInitialized();
         assertion.assertEqual(sts.pageStatus, true, "Browse page status mismatch");
@@ -47,7 +47,7 @@ module.exports = {
 
     //Validate on clicking "Today in Class" resource in Explore menu, same tab is launched on Browse
     ENG_DASH_TC_5: function () {
-        sts = dashboardPage.clickResource("Today in Class");
+        sts = dashboardPage.clickExploreResource("Today in Class");
         assertion.assertEqual(sts, true, "Dashboard page status mismatch");
         sts = browsePage.isInitialized();
         assertion.assertEqual(sts.pageStatus, true, "Browse page status mismatch");
@@ -57,11 +57,39 @@ module.exports = {
 
     //Validate on clicking "Projectable" resource in Explore menu, same tab is launched on Browse
     ENG_DASH_TC_6: function () {
-        sts = dashboardPage.clickResource("Projectable");
+        sts = dashboardPage.clickExploreResource("Projectable");
         assertion.assertEqual(sts, true, "Dashboard page status mismatch");
         sts = browsePage.isInitialized();
         assertion.assertEqual(sts.pageStatus, true, "Browse page status mismatch");
         sts = browsePage.getActiveTabName();
         assertion.assertEqual(sts, "Projectable", "Active tab name status Mismatch");
+    },
+
+    //Validate on clicking "View Classes" from Book dropdown menu, class Pane is launched
+    ENG_DASH_TC_7: function(){
+        sts = dashboardPage.clickMenuViewClass("PROYECTOS");
+        console.log(JSON.stringify(sts));
+        assertion.assertEqual(sts, true);
+    },
+
+    //Validate on clicking "Create Classes" from Book dropdown menu, create class page is launched
+    ENG_DASH_TC_8: function(){
+        sts = dashboardPage.clickMenuCreateClass("PROYECTOS");
+        console.log(JSON.stringify(sts));
+        assertion.assertEqual(sts, true);
+    },
+
+    //Validate on clicking "Remove from My Books" from Book dropdown menu, not active snackbar message is displayed
+    ENG_DASH_TC_9: function(){
+        sts = dashboardPage.clickMenuRemoveBook("PROYECTOS");
+        console.log(JSON.stringify(sts));
+        assertion.assertEqual(sts, true);
+    },
+
+    //Validate on clicking "Open Flipbook" from Book dropdown menu, flipbook page is launched
+    ENG_DASH_TC_10: function(testdata){
+        sts = dashboardPage.clickMenuOpenFlipbook(testdata.bookName);
+        console.log(JSON.stringify(sts));
+        assertion.assertEqual(sts, true);
     }
 };
