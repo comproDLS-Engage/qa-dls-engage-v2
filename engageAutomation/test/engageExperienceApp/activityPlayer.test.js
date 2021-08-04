f8fb08ba1ca61200a90dad68cbb587e9db2594e8
meghagarg <meghagarg.ymca@gmail.com>
2021-08-04 15:44:20 +0530

    feedback

--- engageAutomation/test/engageExperienceApp/activityPlayer.test.js
+++ engageAutomation/test/engageExperienceApp/activityPlayer.test.js
@@ -1,11 +1,6 @@
 "use strict";
-// var launchQuiz = require('../../pages/engageExperienceApp/launchQuiz.page.js');
 var activityPlayerPage = require('../../pages/engageExperienceApp/activityPlayer.page.js');
 var itemPlayer = require('../../pages/engageExperienceApp/itemPlayer.page');
-// var orderlist = require("../../pages/engageExperienceApp/orderlist.page");
-// var summaryPage = require("../../pages/engageExperienceApp/summary.page")
-// var TOC = require('../../pages/engageExperienceApp/toc_learningPath.page.js');
-// var header = require('../../pages/engageExperienceApp/header.page.js');
 var sts;
 
 module.exports = {
@@ -43,21 +38,24 @@ module.exports = {
 	},
 
 	//Validate the test player when user click Previous button in the second question.
-	ENG_PLAY_TC_4: function () {
+	/*ENG_PLAY_TC_4: function () {
 		sts = activityPlayerPage.clickPreviousQuestion();
 		assertion.assertEqual(sts, true, "status mismatch");
 		sts = activityPlayerPage.getActivityPlayerData();
+		//assertions to be added
 	},
 
 	//Validate the test player when user click Show Answer button.
 	ENG_PLAY_TC_5: function () {
 		sts = activityPlayerPage.clickShowAnswer();
 		assertion.assertEqual(sts, true, "status mismatch");
+		//assertions to be added
 	},
 
 	//Validate the test player when user click Your Response.
 	ENG_PLAY_TC_6: function () {
 		sts = activityPlayerPage.clickYourResponse();
 		assertion.assertEqual(sts, true, "status mismatch");
-	}
+		//assertions to be added
+	}*/
 }
