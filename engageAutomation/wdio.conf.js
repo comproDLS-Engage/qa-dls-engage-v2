7c9a4b5ceccb85fa3ce40bbc8aa18cd564d0a19a
Akhil Aggarwal <akhil.aggarwal@comprotechnologies.com>
2021-12-17 11:26:40 +0530

    added more screenshots params for tesults

--- engageAutomation/wdio.conf.js
+++ engageAutomation/wdio.conf.js
@@ -353,9 +353,7 @@ exports.config = {
             const util = require('util');
             const tesultsResults = util.promisify(tesults.results);
             const jsonString = fs.readFileSync(process.cwd() + '/' + global.reportOutputDir + "/wdio-0-0-timeline-reporter.log", 'utf-8');
-            //console.log(process.cwd() + '/' + global.reportOutputDir + "/wdio-0-0-timeline-reporter.log")
             const logData = JSON.parse(jsonString);
-            //console.log(logData)
             var tesultsCases = [];
             for (let i = 0; i < logData.suites.length; i++) {
                 for (let j = 0; j < logData.suites[i].tests.length; j++) {
@@ -363,19 +361,20 @@ exports.config = {
                     testCase.name = logData.suites[i].tests[j].title;
                     testCase.result = logData.suites[i].tests[j].state.substring(0, 4);
                     //console.log(testCase.result)
-                    testCase.duration = logData.suites[i].duration;
+                    testCase.duration = logData.suites[i].tests[j].duration;
+                    testCase.files = logData.suites[i].tests[j].screenshots;
                     testCase.suite = logData.suites[i].title;
                     tesultsCases.push(testCase);
                 }
             }
-            console.log(argv.tesultsToken)
+            //console.log(argv.tesultsToken)
             let data = {
                 target: argv.tesultsToken,
                 results: {
                     cases: tesultsCases
                 }
             }
-            console.log(data)
+            console.log(data.target)
             let response;
             try {
                 response = await tesultsResults(data)
@@ -385,17 +384,6 @@ exports.config = {
                 console.log(response)
             }
         }
-        //console.log(data.results.cases)
-        // tesults.results(data, function (err, response) {
-        //     console.log("inside callback");
-        //     console.log(err)
-        //     console.log(response)
-        //     // err is undefined unless there is a library error
-        //     // response.success is a bool, true if results successfully uploaded, false otherwise
-        //     // response.message is a string, if success is false, check message to see reason
-        //     // response.warnings is an array of strings, if non empty then issues with file uploads
-        //     // response.errors is an array of strings, if success is true this is empty
-        // });
 
         //require('./core/utils/reportUpdater.js').indexFileUpdate();
         specGenerator.removingTempSpecs();
