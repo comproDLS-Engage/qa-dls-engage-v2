e71ace679626c672365cff8ee4604823b22da5f2
Rupsi Mehta <rupsi.mehta@comprotechnologies.com>
2022-04-27 14:48:36 +0530

    Commit some changes

--- engageAutomation/test/engageExperienceApp/assignmentDetails.test.js
+++ engageAutomation/test/engageExperienceApp/assignmentDetails.test.js
@@ -90,7 +90,7 @@ module.exports = {
    ENG_ASSDETAILS_TC_13: function () {
       sts = viewAsStudentAssignmentPage.click_cancel();
       assertion.assertEqual(sts, true, "Cancel button is clicked");
-      res = require('./assignmentDetails.page').isInitialized();
+      res = require('../../pages/engageExperienceApp/assignmentDetails.page').isInitialized();
       assertion.assertEqual(sts.pageStatus, true, "assignment Details page status mismatch");
    },
    ENG_ASSDETAILS_TC_14: function () {
