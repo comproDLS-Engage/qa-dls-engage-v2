f45f0e9854d8a9b9f74cd8fd1ea49fb3084fbb53
Akhil Aggarwal <akhil.aggarwal@comprotechnologies.com>
2022-07-22 17:23:12 +0530

    fib text player updates

--- engageAutomation/pages/engageExperienceApp/FIBText.ItemPlayer.page.js
+++ engageAutomation/pages/engageExperienceApp/FIBText.ItemPlayer.page.js
@@ -1,51 +1,50 @@
 "use strict";
-const action = require('../../core/actionLibrary/baseActionLibrary');
-var selectorFile = jsonParserUtil.jsonParser(selectorDir);
-var res, qIndex;
 const itemplayer = require('./itemPlayer.page.js');
+const action = require('../../core/actionLibrary/baseActionLibrary.js');
+var selectorFile = jsonParserUtil.jsonParser(selectorDir);
 
 module.exports = {
 
-    response: selectorFile.css.ComproEngage.FIBText.response,
-    responses: selectorFile.css.ComproEngage.FIBText.responses,
+    responses: selectorFile.css.ComproEngage.fibtext.responses,
+    textPlaceholder: selectorFile.css.ComproEngage.fibtext.textPlaceholder,
 
-    isInitialized: async function (fibQuesData) {
-        await logger.logInto(await stackTrace.get());
-        var obj = {
-            targetData: []
-        };
-        qIndex = await itemplayer.getQuesIndex();
-        obj.targetData = await this.getTargetData(qIndex, fibQuesData);
-        return obj;
-    },
-
-    getTargetData: async function (qIndex, fibQuesData) {
+    isInitialized: async function () {
         await logger.logInto(await stackTrace.get());
         await action.switchToFrame(0);
-        let option = "div[index='" + qIndex + "'] " + this.responses;
-        var targetArr = [];
-        let optionLength = (await action.findElements(option)).length;
-        for (let i = 0; i < optionLength; i++) {
-            var textSelector = "div[index='" + qIndex + "'] " + this.response + fibQuesData[i][0] + "] ";
-            var value = await itemplayer.getFeedbackIconDetails(textSelector);
-            targetArr[i] = [fibQuesData[i][0], await action.getValue(textSelector + "[data-tid=text-placeholder]"), value];
+        let res = await action.waitForExist(this.responses);
+        await action.switchToParentFrame();
+        return res;
+    },
+
+    getData_responses: async function (responseArr) {
+        await logger.logInto(await stackTrace.get());
+        await action.switchToFrame(0);
+        var arr = [];
+        for (let i = 0; i < responseArr.length; i++) {
+            var textSelector = this.responses + responseArr[i][0];
+            arr[i] = [
+                responseArr[i][0],
+                await action.getValue(textSelector + this.textPlaceholder),
+                await itemplayer.getFeedbackIconDetails(textSelector + "]")];
         }
         await action.switchToParentFrame();
-        return targetArr;
+        return arr;
     },
 
     inputValues: async function (fibQuesData) {
         await logger.logInto(await stackTrace.get());
-        qIndex = await itemplayer.getQuesIndex();
         await action.switchToFrame(0);
+        var textSelector, res;
         for (let i = 0; i < fibQuesData.length; i++) {
-            var textSelector = "div[index='" + qIndex + "'] " + this.response + fibQuesData[i][0] + "] [data-tid=text-placeholder]";
+            textSelector = this.responses + fibQuesData[i][0] + this.textPlaceholder;
             res = await action.click(textSelector);
             await browser.pause(250);
             if (true == res) {
                 res = await action.setValue(textSelector, fibQuesData[i][1]);
                 await browser.pause(250);
                 await logger.logInto(await stackTrace.get(), res);
+                if (res != true)
+                    break;
             }
             else {
                 res = res + " -- Target text placeholder " + fibQuesData[i][0] + " is NOT clicked";
